e5637251db9141758268790fe1808d09c4186ea9
IrakozeLoraine <mukezwa@gmail.com>
2024-09-13 18:42:22 +0400

    fix: error on payload dict type

--- console.py
+++ console.py
@@ -213,7 +213,8 @@ class HBNBCommand(cmd.Cmd):
         if match_json:
             payload = None
             try:
-                payload: dict = json.loads(match_json[0])
+                payload: dict
+                payload = json.loads(match_json[0])
             except Exception:
                 print("** invalid syntax")
                 return
